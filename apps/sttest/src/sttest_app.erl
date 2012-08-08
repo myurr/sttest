1ef3284e3053343fdfa4f566f4b54701c3ab228c
Chris Mellish <chris@pureinnovation.com>
2012-08-08 08:21:30 +0100

    Modified the routing, now works.  Added some optimisations to the run script to enable erlang to be groovy.

--- apps/sttest/src/sttest_app.erl
+++ apps/sttest/src/sttest_app.erl
@@ -14,7 +14,24 @@ start() ->
 
 start(_StartType, _StartArgs) ->
 	io:format("Firing up the listener...~n"),
-	stampede:listen([{port, 8080}], {callback, sttest_handler}, [{idle_workers, 5}]).
+
+	Routes = [
+		{method, 'GET', [
+			{host, <<"localhost">>, [
+				{set_path, <<"/www/sites/test/">>},
+				{url, <<"/static">>, [
+					{path, <<"htdocs/">>},
+					{static_dir, <<"index.html">>}
+				]}
+			]}
+		]},
+		{method, 'ERROR', [
+			{url, <<"404">>, [{static, <<"Err we seem to be missing something here Bert.">>}]},
+			{url, <<"500">>, [{static, <<"Total failure!!">>}]}
+		]}
+	],
+
+	stampede:listen([{port, 8080}], Routes, [{idle_workers, 5}]).
 
 stop(_State) ->
 	io:format("Oh... I've died.  Bugger...~n"),
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl -pa apps/sttest/ebin -pa deps/stampede/ebin -s stampede -s sttest_app
+erl +K true +A 16 -pa apps/sttest/ebin -pa deps/stampede/ebin -s stampede -s sttest_app
