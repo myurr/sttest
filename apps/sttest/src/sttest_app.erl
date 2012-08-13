12e8696826f4c6132c88a14fe4658f4c28304b7a
Chris Mellish <chris@pureinnovation.com>
2012-08-13 09:04:04 +0100

    Latest

--- apps/sttest/src/sttest_app.erl
+++ apps/sttest/src/sttest_app.erl
@@ -10,28 +10,35 @@
 %% ===================================================================
 
 start() ->
-	application:start(sttest).
+	ok = application:start(sttest).
 
 start(_StartType, _StartArgs) ->
 	io:format("Firing up the listener...~n"),
 
 	Routes = [
-		{method, 'GET', [
-			{host, <<"localhost">>, [
+		{host, [<<"localhost">>, <<"stampede.pureinnovation.com">>], [
+			{method, 'GET', [
 				{set_path, <<"/www/sites/test/">>},
+				{browser_cache_for, {{0, 0, 1}, {0, 0, 0}}},
+				{map_file, <<"/favicon.ico">>, <<"htdocs/favicon.ico">>, []},
+	%			{set_site, testsite},
+	%			{session, <<"sid">>, <<"">>},
 				{url, <<"/static">>, [
 					{path, <<"htdocs/">>},
 					{static_dir, <<"index.html">>, []}
 				]}
+			]},
+			{method, 'ERROR', [
+%				{url, <<"404">>, [{static, <<"Err we seem to be missing something here Bert.">>}]},
+%				{url, <<"500">>, [{static, <<"Total failure!!">>}]}
 			]}
-		]},
-		{method, 'ERROR', [
-%			{url, <<"404">>, [{static, <<"Err we seem to be missing something here Bert.">>}]},
-%			{url, <<"500">>, [{static, <<"Total failure!!">>}]}
 		]}
 	],
 
-	stampede:listen([{port, 8080}], Routes, [{idle_workers, 5}]).
+	application:start(stampede),
+	% stampede:nodes([]),
+	% stampede:create_site(testsite, [{tmpdir, <<"/tmp/stampede/testsite">>}]),
+	stampede:listen([{port, 8080}], Routes, [{idle_workers, 200}]).
 
 stop(_State) ->
 	io:format("Oh... I've died.  Bugger...~n"),
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl +K true +A 16 -pa apps/sttest/ebin -pa deps/stampede/ebin -s stampede -s sttest_app
+erl +K true +A 50 -env ERL_MAX_PORTS 50000 -pa apps/sttest/ebin -pa deps/stampede/ebin -s sttest_app
